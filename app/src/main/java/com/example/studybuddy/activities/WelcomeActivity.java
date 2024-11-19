baeb0ab46f8fd430d505e6fd90980a311b0c6380
heyanlu <he.yanl@northeastern.edu>
2024-11-19 11:44:45 -0800

    Added Session to avoid repeat login

--- app/src/main/java/com/example/studybuddy/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/studybuddy/activities/WelcomeActivity.java
@@ -1,26 +1,43 @@
 package com.example.studybuddy.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studybuddy.R;
+import com.example.studybuddy.data.database.DatabaseHelper;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-    private static final int WELCOME_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
 
-        new Handler().postDelayed(() -> {
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        String savedEmail = sharedPreferences.getString("userEmail", null);
+
+        if (savedEmail != null) {
+            navigateToNextActivity(savedEmail);
+        } else {
             Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
             startActivity(intent);
-            finish();
-        }, WELCOME_DISPLAY_LENGTH);
+        }
+
+        finish();
+    }
+
+    private void navigateToNextActivity(String email) {
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        boolean isSetUp = dbHelper.isSetUp(email);
+
+        Intent intent;
+        if (isSetUp) {
+            intent = new Intent(WelcomeActivity.this, MatchUserActivity.class);
+        } else {
+            intent = new Intent(WelcomeActivity.this, UserProfileActivity.class);
+        }
+        startActivity(intent);
     }
 }
