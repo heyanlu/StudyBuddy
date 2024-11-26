161d24e956ddd7876a4c0577ce10d63f53cce453
Aarzoo-Bansal <aarzoo1701@gmail.com>
2024-11-25 20:26:43 -0800

    sign-up page changes
    # Conflicts:
    #	app/src/main/res/layout/activity_match_user.xml

--- app/src/main/java/com/example/studybuddy/activities/SignupActivity.java
+++ app/src/main/java/com/example/studybuddy/activities/SignupActivity.java
@@ -2,6 +2,9 @@ package com.example.studybuddy.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,10 +15,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.studybuddy.R;
 import com.example.studybuddy.data.database.DatabaseHelper;
 import com.example.studybuddy.utils.ValidationUtils;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignupActivity extends AppCompatActivity {
-    private EditText editTextEmail, editTextPassword;
+    private TextInputEditText editTextEmail;
     private DatabaseHelper dbHelper;
+    private boolean isPasswordVisible = false; //Initializing to false as initially the password is not visible
+    private boolean isConfirmPasswordVisible = false; //Initializing to false as initially the password is not visible
+    private Button buttonSignup;
+    private TextInputLayout passwordLayout;
+    private TextInputEditText passwordEditText;
+
+    private TextInputLayout confirmPasswordLayout;
+    private TextInputEditText confirmPasswordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +39,30 @@ public class SignupActivity extends AppCompatActivity {
         dbHelper = new DatabaseHelper(this);
 
         editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        Button buttonSignup = findViewById(R.id.buttonSignup);
-        TextView textViewLogin = findViewById(R.id.textViewLogin);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        buttonSignup = findViewById(R.id.signup_button);
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+       // TextView textViewLogin = findViewById(R.id.textViewLogin);
 
 
         buttonSignup.setOnClickListener(view -> {
             String email = editTextEmail.getText().toString().trim();
-            String password = editTextPassword.getText().toString();
+            String password = passwordEditText.getText().toString().trim();
+            String confirmPassword = confirmPasswordEditText.getText().toString().trim();
 
-            if (!ValidationUtils.isValidEmail(email)) {
+            if (!ValidationUtils.isValidEmail(email)){
                 Toast.makeText(SignupActivity.this, "Invalid email format", Toast.LENGTH_SHORT).show();
-                return;
+               return;
             }
+
 
             if (!ValidationUtils.isValidPassword(password)) {
                 Toast.makeText(SignupActivity.this, "Password must be at least 6 characters, include uppercase, lowercase, and a number", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if(!password.equals(confirmPassword)){
+                Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
                 return;
             }
 
@@ -55,14 +77,60 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-        textViewLogin.setOnClickListener(view -> {
-            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+//        textViewLogin.setOnClickListener(view -> {
+//            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+//            startActivity(intent);
+//            finish();
+ //       });
+
+        //The below code helps to toggle the password field
+        passwordLayout = findViewById(R.id.password_layout);
+        passwordEditText = findViewById(R.id.editTextPassword);
+
+        confirmPasswordLayout = findViewById(R.id.confirmPasswordLayout);
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+
+        passwordLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            final String fieldName = getString(R.string.password);
+
+            @Override
+            public void onClick(View v) {
+                togglePasswordVisibility(isPasswordVisible, passwordLayout, passwordEditText, fieldName);
+            }
+        });
+
+        confirmPasswordLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            final String fieldName = getString(R.string.confirm_password);
+
+            @Override
+            public void onClick(View v) {
+                togglePasswordVisibility(isConfirmPasswordVisible, confirmPasswordLayout, confirmPasswordEditText, fieldName);
+            }
         });
 
 
 
 
+
     }
+
+    public void togglePasswordVisibility(Boolean isPasswordVisible, TextInputLayout layout, TextInputEditText editText, String fieldName) {
+        isPasswordVisible = !isPasswordVisible;
+
+        if (fieldName.equals(getString(R.string.password))) {
+            this.isPasswordVisible = !this.isPasswordVisible;
+        } else {
+            this.isConfirmPasswordVisible = !this.isConfirmPasswordVisible;
+        }
+
+        if (isPasswordVisible) { //This means that password is currently visible and we need to hide it
+            editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            layout.setEndIconDrawable(R.drawable.baseline_visibility_off_24);
+        } else { //Password is hidden and we need to show it
+            editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            layout.setEndIconDrawable(R.drawable.baseline_visibility_24);
+        }
+        editText.setSelection(editText.getText().length());
+    }
+
 }
